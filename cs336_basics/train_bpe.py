169745a8ba2311cd4f5fa6c7827ca64a61bec94e
edenni <ntj0710@gmail.com>
2026-01-07 10:11:11 +0800

    optimize merging

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -20,53 +20,60 @@ def get_most(counts: dict[tuple[bytes], int]) -> tuple[tuple[bytes, bytes], int]
     return max(ties), max_count
 
 
-def decode(tokens):
-    chars = []
-    for t in tokens:
-        try:
-            if t > 255:
-                raise
-            t = chr(t)
-        except:
-            pass
-        chars.append(t)
-    return chars
-
-
-def merge(pretoken_counts: dict[tuple[bytes], int], tokens_to_merge: tuple[bytes, bytes]) -> dict[tuple[bytes], int]:
+def merge(
+    pre_token_counts: dict[tuple[bytes], int],
+    token_pair_counts: dict[tuple[bytes, bytes], int],
+    tokens_to_merge: tuple[bytes, bytes],
+) -> dict[tuple[bytes], int]:
     """Merge the token pair with max count."""
     left, right = tokens_to_merge
     merged_token = left + right
 
-    for tokens, count in pretoken_counts.copy().items():
+    for tokens, count in pre_token_counts.copy().items():
         new_tokens = []
         updated = False
         i = 0
+
         while i < len(tokens):
             if i + 1 < len(tokens) and tokens[i] == left and tokens[i + 1] == right:
                 new_tokens.append(merged_token)
                 updated = True
+
+                # update pair counts
+                token_pair_counts[tokens_to_merge] -= count
+                if i > 0:
+                    token_pair_counts[(tokens[i - 1], left)] -= count
+                    token_pair_counts[(tokens[i - 1], merged_token)] += count
+                if i + 2 < len(tokens):
+                    token_pair_counts[(right, tokens[i + 2])] -= count
+                    token_pair_counts[(merged_token, tokens[i + 2])] += count
                 i += 2
             else:
                 new_tokens.append(tokens[i])
                 i += 1
         if updated:
-            pretoken_counts[tuple(new_tokens)] = pretoken_counts.pop(tokens)
-    return pretoken_counts
+            pre_token_counts[tuple(new_tokens)] = pre_token_counts.pop(tokens)
+    return pre_token_counts
 
 
-def one_step(pretoken_counts: dict[tuple[bytes], int], vocab: dict[int, bytes]):
-    # count byte pairs
-    token_pair_counts = defaultdict(int)
-    for pretoken, count in pretoken_counts.items():
-        for i in range(len(pretoken) - 1):
-            token_pair_counts[pretoken[i : i + 2]] += count
+def _one_step(
+    vocab: dict[int, bytes],
+    pre_token_counts: dict[tuple[bytes], int],
+    token_pair_counts: dict[tuple[bytes, bytes], int] = None,
+):
+    # init token pair counts
+    if token_pair_counts is None:
+        token_pair_counts = defaultdict(int)
+        for pre_token, count in pre_token_counts.items():
+            for i in range(len(pre_token) - 1):
+                token_pair_counts[pre_token[i : i + 2]] += count
+
     tokens_to_merge, _ = get_most(token_pair_counts)
 
     # merge
-    pretoken_counts = merge(pretoken_counts, tokens_to_merge)
+    pre_token_counts = merge(pre_token_counts, token_pair_counts, tokens_to_merge)
     vocab[len(vocab)] = tokens_to_merge[0] + tokens_to_merge[1]
-    return pretoken_counts, vocab, tokens_to_merge
+    return vocab, pre_token_counts, token_pair_counts, tokens_to_merge
 
 
 def train_bpe(
@@ -78,18 +85,19 @@ def train_bpe(
         vocab[i + 256] = token.encode("utf-8")
 
     # pre-tokenization
-    pretoken_counts = pretokenize(input_path, 1)
-    pretoken_counts: dict[tuple[bytes], int] = {
-        tuple(bytes([b]) for b in k.encode("utf8")): v for k, v in pretoken_counts.items()
+    pre_token_counts = pretokenize(input_path, 1)
+    pre_token_counts: dict[tuple[bytes], int] = {
+        tuple(bytes([b]) for b in k.encode("utf8")): v for k, v in pre_token_counts.items()
     }
 
     # merge
-    merges = []
+    merges: list[tuple[bytes, bytes]] = []
+    token_pair_counts = None
     for i in tqdm(range(vocab_size - len(vocab))):
-        pretoken_counts, vocab, merged = one_step(pretoken_counts, vocab)
+        vocab, pre_token_counts, token_pair_counts, merged = _one_step(vocab, pre_token_counts, token_pair_counts)
         merges.append(merged)
     return vocab, merges
 
 
 if __name__ == "__main__":
-    train_bpe("../data/TinyStoriesV2-GPT4-valid.txt", 258, ["<|endoftext|>"])
+    train_bpe("./data/TinyStoriesV2-GPT4-valid.txt", 258, ["<|endoftext|>"])
