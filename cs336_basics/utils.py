4efbb728ffbf0b20624ef4bc61fb0e94c6865cdd
edenni <ntj0710@gmail.com>
2026-01-09 09:08:09 +0800

    checkpoint

---
+++ cs336_basics/utils.py
@@ -0,0 +1,19 @@
+import torch
+
+
+def save_checkpoint(model, optimizer, iteration, out):
+    torch.save(
+        {
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "iteration": iteration,
+        },
+        out,
+    )
+
+
+def load_checkpoint(src, model, optimizer):
+    state_dict = torch.load(src)
+    model.load_state_dict(state_dict["model"])
+    optimizer.load_state_dict(state_dict["optimizer"])
+    return state_dict["iteration"]
