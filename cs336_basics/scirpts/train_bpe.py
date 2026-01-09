e3c83068ea9fe35134860bfd1949a78ee7e39da0
edenni <ntj0710@gmail.com>
2026-01-09 11:38:13 +0800

    save vocab and merges

--- cs336_basics/scirpts/train_bpe.py
+++ cs336_basics/scirpts/train_bpe.py
@@ -3,18 +3,19 @@ import cProfile
 import wandb
 
 from cs336_basics.train_bpe import train_bpe
+from cs336_basics.utils import save_voacb_and_merge
 
-text_source = "data/TinyStoriesV2-GPT4-train.txt"
-output_vocab_path = "data/out/tinystories_vocab.json"
-output_merge_path = "data/out/tinystories_merges.txt"
+input_path = "data/TinyStoriesV2-GPT4-train.txt"
+output_vocab_path = "outputs/tinystories_vocab.json"
+output_merge_path = "outputs/tinystories_merges.txt"
 
 wandb_name = "cs336_basics"
 wandb_run_name = "train_bpe_tinystories"
 wandb_logging = False
 
-special_tokens = ["<|endoftext|>"]
 vocab_size = 10000
-num_workers = 20
+special_tokens = ["<|endoftext|>"]
+num_processes = 24
 
 config_keys = [k for k, v in globals().items() if not k.startswith("_") and isinstance(v, (int, float, bool, str))]
 config = {k: globals()[k] for k in config_keys}
@@ -24,7 +25,7 @@ if wandb_logging:
 
 pr = cProfile.Profile()
 pr.enable()
-vocab, merges = train_bpe(text_source, vocab_size, special_tokens, progress_bar=True, num_workers=num_workers)
+vocab, merges = train_bpe(input_path, vocab_size, special_tokens, num_processes=num_processes)
 pr.disable()
-
 pr.print_stats(sort="time")
+save_voacb_and_merge(vocab, merges, output_vocab_path, output_merge_path)
--- cs336_basics/utils.py
+++ cs336_basics/utils.py
@@ -1,4 +1,86 @@
+import json
+import os
+from functools import lru_cache
+
 import torch
+
+
+@lru_cache
+def gpt2_bytes_to_unicode():
+    """
+    https://github.com/openai/gpt-2/blob/master/src/encoder.py#L9
+
+    Returns list of utf-8 byte and a corresponding list of unicode strings.
+    The reversible bpe codes work on unicode strings.
+    This means you need a large # of unicode characters in your vocab if you want to avoid UNKs.
+    When you're at something like a 10B token dataset you end up needing around 5K for decent coverage.
+    This is a signficant percentage of your normal, say, 32K bpe vocab.
+    To avoid that, we want lookup tables between utf-8 bytes and unicode strings.
+    And avoids mapping to whitespace/control characters the bpe code barfs on.
+    """
+    bs = list(range(ord("!"), ord("~") + 1)) + list(range(ord("¡"), ord("¬") + 1)) + list(range(ord("®"), ord("ÿ") + 1))
+    cs = bs[:]
+    n = 0
+    for b in range(2**8):
+        if b not in bs:
+            bs.append(b)
+            cs.append(2**8 + n)
+            n += 1
+    cs = [chr(n) for n in cs]
+    return dict(zip(bs, cs))
+
+
+def get_tokenizer_from_vocab_merges_path(vocab_path: str | os.PathLike, merges_path: str | os.PathLike):
+    gpt2_byte_decoder = {v: k for k, v in gpt2_bytes_to_unicode().items()}
+    with open(vocab_path) as vocab_f:
+        gpt2_vocab = json.load(vocab_f)
+    gpt2_bpe_merges = []
+    with open(merges_path) as f:
+        for line in f:
+            cleaned_line = line.rstrip()
+            if cleaned_line and len(cleaned_line.split(" ")) == 2:
+                gpt2_bpe_merges.append(tuple(cleaned_line.split(" ")))
+    # The GPT-2 tokenizer uses a remapped unicode encoding for bytes. Let's
+    # just return the original bytes, so we don't force students to use
+    # any particular encoding scheme.
+    vocab = {
+        gpt2_vocab_index: bytes([gpt2_byte_decoder[token] for token in gpt2_vocab_item])
+        for gpt2_vocab_item, gpt2_vocab_index in gpt2_vocab.items()
+    }
+
+    merges = [
+        (
+            bytes([gpt2_byte_decoder[token] for token in merge_token_1]),
+            bytes([gpt2_byte_decoder[token] for token in merge_token_2]),
+        )
+        for merge_token_1, merge_token_2 in gpt2_bpe_merges
+    ]
+    return vocab, merges
+
+
+def save_voacb_and_merge(vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], vocab_path: str, merges_path: str):
+    byte_to_unicode = gpt2_bytes_to_unicode()
+
+    # Reverse the mapping from unicode characters to bytes
+    unicode_to_byte = {v: k for k, v in byte_to_unicode.items()}
+
+    # Convert the byte tokens in the vocab back to string tokens using the unicode mapping
+    reversed_vocab = {"".join([byte_to_unicode[b] for b in bytes_token]): k for k, bytes_token in vocab.items()}
+
+    # Convert the byte sequences in merges back to string tokens
+    reversed_merges = [
+        " ".join(["".join([byte_to_unicode[b] for b in merge[0]]), "".join([byte_to_unicode[b] for b in merge[1]])])
+        for merge in merges
+    ]
+
+    # Save the vocab dictionary as a JSON file
+    with open(vocab_path, "w", encoding="utf-8") as f:
+        json.dump(reversed_vocab, f, ensure_ascii=False)
+
+    # Save the merges list to a file
+    with open(merges_path, "w", encoding="utf-8") as f:
+        for merge in reversed_merges:
+            f.write(merge + "\n")
 
 
 def save_checkpoint(model, optimizer, iteration, out):
